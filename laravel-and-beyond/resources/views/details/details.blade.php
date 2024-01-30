e4bc8db0ed7e22a3e1d6fc7e0c32c4509ffc6bb5
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-30 17:06:54 +0100

    move update and delete buttons to details page

--- laravel-and-beyond/resources/views/details/details.blade.php
+++ laravel-and-beyond/resources/views/details/details.blade.php
@@ -12,13 +12,21 @@
                 <div class="card-body">
                     <h5 class="card-title text-3xl mt-2 text-light">{{ $product->title }}</h5>
                     <p class="card-text text-light">{{ $product->price }}</p>
+                    <br>
+                    <a href="{{ route('edit', ['id' => $product->id]) }}" class="bg-yellow-500 text-white py-2 px-4 rounded-md">Update</a>
+                    <form action="{{ route('destroy', ['id' => $product->id]) }}" method="post" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded-md" onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
+                        <br>
+                    </form>
                 </div>
             </div>
         @else
             <p>Product not found.</p>
         @endif
     </div>
-
+<br><br>
     <div class="text-center mt-4">
         <button class="bg-gray-800 text-white py-2 px-4 rounded-md" onclick="window.location.href='{{ route('show.home') }}'">Back</button>
     </div>
--- laravel-and-beyond/resources/views/show/home.blade.php
+++ laravel-and-beyond/resources/views/show/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     @if(session('success'))
-        <div class="bg-green-500 text-white p-4 mb-4">
+        <div class="bg-green-500 bg-opacity-75 text-white p-4 mb-4">
             {{ session('success') }}
         </div>
     @endif
@@ -20,18 +20,12 @@
                     <div class="product">
                         <br>
                         <div class="card bg-dark mb-3 mx-auto">
-                            <img src="{{ asset('storage/' . $product->photo) }}" class="card-img-top custom-thumbnail mt-4" alt="{{ $product->title }}">
+                            <a href="{{ route('details', ['id' => $product->id]) }}">
+                                <img src="{{ asset('storage/' . $product->photo) }}" class="card-img-top custom-thumbnail mx-auto mt-4" alt="{{ $product->title }}">
+                            </a>
                             <div class="card-body">
                                 <h5 class="card-title text-light">{{ $product->title }}</h5>
                                 <p class="card-text text-light">{{ $product->price }}</p>
-                                <a href="{{ route('details', ['id' => $product->id]) }}" class="bg-blue-500 text-white py-2 px-4 rounded-md">Details</a>
-                                <a href="{{ route('edit', ['id' => $product->id]) }}" class="bg-yellow-500 text-white py-2 px-4 rounded-md">Update</a>
-                                <form action="{{ route('destroy', ['id' => $product->id]) }}" method="post" style="display: inline-block;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="bg-red-500 text-white py-2 px-4 rounded-md" onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
-                                    <br>
-                                </form>
                             </div>
                         </div>
                     </div>
@@ -49,4 +43,3 @@
     </div>
 
 @endsection
-
