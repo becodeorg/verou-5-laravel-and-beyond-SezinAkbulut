4e118835d317fe3256977f3172f9fce8dbd2309f
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-30 12:35:29 +0100

    create RegisterController and UserController

--- laravel-and-beyond/app/Http/Controllers/RegisterController.php
+++ laravel-and-beyond/app/Http/Controllers/RegisterController.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view("sessions.register");
+    }
+
+    public function create(Request $request)
+    {
+        // Validate the form data
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        // Create a new user
+        User::create([
+            'name' => $request->input('name'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        // Redirect with a success message
+        return redirect()->route('show.home')->with('success', 'You registered successfully!');
+    }
+}
--- laravel-and-beyond/app/Http/Controllers/UserController.php
+++ laravel-and-beyond/app/Http/Controllers/UserController.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return view("users.index", [
+            "users" => $users,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view("users.show", [
+            "user" => $user,
+        ]);
+    }
+}
